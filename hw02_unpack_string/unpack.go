0a5a0a325af5410538f405b62136be5720dac29d
Ilya Chelmodeev <ilya.chelmodeev@vitech.team>
2023-10-15 21:14:42 +0300

    homework №2 almost done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,34 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if _, err := strconv.Atoi(s); err == nil {
+		return "", ErrInvalidString
+	}
+
+	var prev rune
+	var escaped bool
+	var b strings.Builder
+	for _, char := range s {
+		if unicode.IsDigit(char) && !escaped {
+			m := int(char - '0')
+			r := strings.Repeat(string(prev), m-1)
+			b.WriteString(r)
+		} else {
+			escaped = string(char) == "\\" && string(prev) != "\\"
+			if !escaped {
+				b.WriteRune(char)
+			}
+			prev = char
+		}
+	}
+
+	return b.String(), nil
 }
