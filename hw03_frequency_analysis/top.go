5bbbae735538d6f1249f288788161cfd37b9d1be
Ilya Chelmodeev <ilya.chelmodeev@vitech.team>
2024-02-04 23:03:45 +0300

    hw03 done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordCountStruct struct {
+	w string
+	c int32
+}
+
+const TopNumber = 10
+
+var globalRegexp = regexp.MustCompile("[a-яА-Я-]+")
+
+func getTokenFrequency(inp string) map[string]int32 {
+	tokenFrequency := make(map[string]int32)
+	tokens := strings.Fields(inp)
+	for _, t := range tokens {
+		if globalRegexp.MatchString(t) {
+			tokenFrequency[t]++
+		}
+	}
+	return tokenFrequency
+}
+
+func getWordCounts(tokenFrequency map[string]int32) []wordCountStruct {
+	var wordCounts []wordCountStruct
+	for key, val := range tokenFrequency {
+		wordCounts = append(wordCounts, wordCountStruct{w: key, c: val})
+	}
+
+	sort.Slice(wordCounts, func(i, j int) bool {
+		if wordCounts[i].c == wordCounts[j].c {
+			return wordCounts[i].w < wordCounts[j].w
+		}
+		return wordCounts[i].c > wordCounts[j].c
+	})
+
+	return wordCounts
+}
+
+func getTopWords(wordCounts []wordCountStruct, topN int) []string {
+	result := make([]string, topN)
+	for i, curWordCount := range wordCounts[:topN] {
+		result[i] = curWordCount.w
+	}
+	return result
+}
+
+func Top10(inp string) []string {
+	tokenFrequency := getTokenFrequency(inp)
+	wordCounts := getWordCounts(tokenFrequency)
+	maxRetLen := math.Min(float64(TopNumber), float64(len(wordCounts)))
+	return getTopWords(wordCounts, int(maxRetLen))
 }
